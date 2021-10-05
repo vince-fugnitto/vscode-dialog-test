32e3d4cd8060748e5702162f73615190bdbf1905
vince-fugnitto <vincent.fugnitto@ericsson.com>
2021-10-05 09:54:00 -0400

    ext: add `item`
    Signed-off-by: vince-fugnitto <vincent.fugnitto@ericsson.com>

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,11 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
     let disposable = vscode.commands.registerCommand('vscode-dialog.dialog', () => {
         const message = 'The following is an example modal dialog.';
-        vscode.window.showInformationMessage(message, { modal: true, detail: 'detail content' });
+        const items = ['Test'];
+        vscode.window.showInformationMessage(message, {
+            modal: true,
+            detail: 'detail content'
+        }, ...items);
     });
     context.subscriptions.push(disposable);
 }
