9ef542a72867124346780cd4d12460d9796269a8
vince-fugnitto <vincent.fugnitto@ericsson.com>
2021-10-13 11:48:25 -0400

    ext: add dialog with significant content
    Signed-off-by: vince-fugnitto <vincent.fugnitto@ericsson.com>

--- src/extension.ts
+++ src/extension.ts
@@ -6,6 +6,15 @@ export function activate(context: vscode.ExtensionContext) {
     // Information modal dialog.
     disposables.push(vscode.commands.registerCommand('vscode-dialog.info-dialog', () => openModalDialog('info')));
 
+    // Information modal dialog with a lot of content.
+    disposables.push(vscode.commands.registerCommand('vscode-dialog.info-dialog-content', () => {
+        const message = `The following is an example of a modal dialog with a significant amount of content\nThe dialog should properly display.`;
+        const detail = `The following is a detail example with a significant amount of content\nThe dialog should properly display.`;
+        const items = ['Test'];
+        const options: vscode.MessageOptions = { modal: true, detail };
+        vscode.window.showInformationMessage(message, options, ...items);
+    }));
+
     // Warning modal dialog.
     disposables.push(vscode.commands.registerCommand('vscode-dialog.warning-dialog', () => openModalDialog('warning')));
 
